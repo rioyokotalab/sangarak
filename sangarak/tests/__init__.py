bdaf2c8b14da2f194984b6aa9e9e29470b326624
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-08-24 11:37:10 +0900

    add tests for 16x16 4x4  block cyclic matrix

--- sangarak/tests/__init__.py
+++ sangarak/tests/__init__.py
@@ -6,9 +6,31 @@ import sangarak
 
 fixtures = "sangarak/tests/fixtures/"
 
-class Test16x16Matrix(TestCase):
+class Test_16x16_4x4_2x2_Matrix(TestCase):
     def __init__(self, *args, **kwargs):
-        super(Test16x16Matrix, self).__init__(*args, **kwargs)
+        super(Test_16x16_4x4_2x2_Matrix, self).__init__(*args, **kwargs)
+        self.path = os.path.abspath(fixtures + "/16x16_4x4_2x2/") + "/"
+        self.matrix = np.arange(16*16).reshape(16,16)
+        self.matrix[np.diag_indices_from(self.matrix)] += 5000
+        self.n = 16
+        self.nb = 8
+        self.pb = 2
+        self.num_procs = 4
+
+    def test_read_matrix(self):
+        files = {
+            0: self.path + "0input.txt",
+            1: self.path + "1input.txt",
+            2: self.path + "2input.txt",
+            3: self.path + "3input.txt"
+        }
+        m = sangarak.read_matrix(files, "row", self.n, self.nb, self.pb, self.num_procs)
+        print(m)
+        self.assertTrue(np.array_equal(self.matrix, m))
+            
+class Test_16x16_8x8_2x2_Matrix(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Test_16x16_8x8_2x2_Matrix, self).__init__(*args, **kwargs)
         self.path = os.path.abspath(fixtures + "/16x16_8x8_2x2/") + "/"
         self.matrix = np.array([
             [5383., 886., 777., 915., 383., 886., 777., 915., 793.,  335., 386.,
