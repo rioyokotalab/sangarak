d341b15d111023240a10be81b714f98b4e7d8492
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-09-03 10:23:43 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='sangarak',
     version='0.1',
     description='Python library for reading matrices from many files and generating a single numpy matrix',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='BSD 3-clause',
-    packages=['sangarak'],
+    packages=find_packages(),
     author='Sameer Deshmukh',
     author_email='sameer.desmukh93@gmail.com',
     keywords=['numpy', 'mpi', 'matrix'],
@@ -15,5 +20,9 @@ setup(
     url='https://github.com/rioyokotalab/sangarak',
     test_suite='nose.collector',
     tests_require=['nose'],
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: BSD 3-clause"
+    ]
 )
